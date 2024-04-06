f850b9fd725209206e4c8daca4c8db05e30e58e6
Richard Basson <dvtrb@utilita.co.uk>
2024-04-06 22:08:10 +0200

    fix resource groups name

--- modules/azurerm/resource_group/main.tf
+++ modules/azurerm/resource_group/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "az_rg" {
 
-  name     = "${var.company_short_code}-${var.product}-${var.environment_name}--${var.name}-rg"
+  name     = "${var.company_short_code}-${var.product}-${var.environment_name}-${var.name}-rg"
   location = var.location
   tags     = var.tags
 }
