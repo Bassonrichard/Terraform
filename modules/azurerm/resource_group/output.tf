c46717723f7824221236cafea8f989e0e3671acd
Richard Basson <dvtrb@utilita.co.uk>
2024-04-06 22:15:46 +0200

    added resource group name as output

--- modules/azurerm/resource_group/output.tf
+++ modules/azurerm/resource_group/output.tf
@@ -2,3 +2,8 @@ output "id" {
   value = azurerm_resource_group.az_rg.id
   description = "The id of the resource group."
 }
+
+output "name" {
+  value = azurerm_resource_group.az_rg.name
+  description = "The name of the resource group."
+}
