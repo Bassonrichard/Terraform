062c7d529b3acf2e3176d8b7a13df8207ffec0b7
Richard Basson <Bassonrichard@gmail.com>
2024-04-10 21:23:03 +0200

    Update output.tf

--- modules/azurerm/container_registry/output.tf
+++ modules/azurerm/container_registry/output.tf
@@ -7,3 +7,8 @@ output "login_server" {
   value       = azurerm_container_registry.az_container_registry.login_server
   description = "The login server of the container registry."
 }
+
+output "name" {
+  value       = azurerm_container_registry.az_container_registry.name
+  description = "The name of the container registry."
+}
