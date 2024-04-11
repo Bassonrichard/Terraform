6a416a5ace6613baa1bccc4b489c0d171954d650
Richard Basson <dvtrb@utilita.co.uk>
2024-04-11 21:35:34 +0200

    Refactor resource group naming in main.tf and variables.tf

--- modules/azurerm/resource_group/main.tf
+++ modules/azurerm/resource_group/main.tf
@@ -1,6 +1,10 @@
+locals {
+  name = var.product == null ? "${var.company_short_code}-${var.environment_name}-${var.name}-rg" : "${var.company_short_code}-${var.product}-${var.environment_name}-${var.name}-rg"
+}
+
 resource "azurerm_resource_group" "az_rg" {
 
-  name     = "${var.company_short_code}-${var.product}-${var.environment_name}-${var.name}-rg"
+  name     = local.name
   location = var.location
   tags     = var.tags
 }
--- modules/azurerm/resource_group/variables.tf
+++ modules/azurerm/resource_group/variables.tf
@@ -8,11 +8,6 @@ variable "location" {
   description = "(Required) Specifies the location of the resource group."
 }
 
-variable "product" {
-  type        = string
-  description = "(Required) The name of product."
-}
-
 variable "company_short_code" {
   type        = string
   description = "(Required) The short code of the company."
@@ -21,6 +16,13 @@ variable "company_short_code" {
 variable "environment_name" {
   type        = string
   description = "(Required) The name of the environmnet the resource group resides in."
+}
+
+variable "product" {
+  type        = string
+  description = "(Optional) The name of product."
+
+  nullable = true
 }
 
 variable "tags" {
