bfc8c806a9f1ef21680408dbf8b72ab8d3404e43
Richard Basson <dvtrb@utilita.co.uk>
2024-05-13 21:16:18 +0200

    feat: Add output for Azure Search Service URL and query keys

--- modules/azurerm/search_service/output.tf
+++ modules/azurerm/search_service/output.tf
@@ -5,3 +5,11 @@ output "id" {
 output "primary_key" {
   value = azurerm_search_service.search_service.primary_key
 }
+
+output "url" {
+  value = "https://${azurerm_search_service.search_service.name}.search.windows.net"
+}
+
+output "query_keys" {
+  value = azurerm_search_service.search_service.query_keys
+}
