0a641de40f544c968a5384b3278ca8e7aaa811ff
Richard Basson <dvtrb@utilita.co.uk>
2024-05-14 09:39:50 +0200

    Update Azure Search Service output to include descriptions and sensitive flags

--- modules/azurerm/search_service/output.tf
+++ modules/azurerm/search_service/output.tf
@@ -1,15 +1,24 @@
 output "id" {
   value = azurerm_search_service.search_service.id
+  description = "The ID of the Search Service."
+
 }
 
 output "primary_key" {
   value = azurerm_search_service.search_service.primary_key
+  description = "The Primary Key of the Search Service."
+
+  sensitive = true
 }
 
 output "url" {
   value = "https://${azurerm_search_service.search_service.name}.search.windows.net"
+  description = "The URL of the Search Service."
 }
 
 output "query_key" {
   value = azurerm_search_service.search_service.query_keys.0.key
+  description = "The Query Key of the Search Service."
+
+  sensitive = true
 }
