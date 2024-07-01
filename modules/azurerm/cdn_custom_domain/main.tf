6535b1c52739c20e4b7b8d2da4ab368e42f5ffa2
Richard Basson <dvtrb@utilita.co.uk>
2024-07-01 23:08:04 +0200

    dedicated cdn custom domain https

--- modules/azurerm/cdn_custom_domain/main.tf
+++ modules/azurerm/cdn_custom_domain/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_cdn_endpoint_custom_domain" "custom_domain" {
   host_name       = "${var.sub_domain}.${var.domain_name}"
 
   cdn_managed_https {
-    certificate_type = "Shared"
+    certificate_type = "Dedicated"
     protocol_type = "ServerNameIndication"
   }
 }
