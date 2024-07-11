6576af2d2bfff735b3df05866e0f4a7e98e65028
Richard Basson <dvtrb@utilita.co.uk>
2024-07-11 20:58:51 +0200

    refactor: Update storage_account_type, sku_name, and administrator_login variables in azurerm/mssql_database module

--- modules/azurerm/mssql_database/main.tf
+++ modules/azurerm/mssql_database/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_mssql_server" "az_mssql_server" {
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = var.db_server.version
-  storage_account_type         = var.db_server.storage_account_type
+  zone_redundant               = var.db_server.zone_redundant
   minimum_tls_version          = var.db_server.minimum_tls_version
   administrator_login          = var.db_server.administrator_login
   administrator_login_password = random_password.password.result
@@ -27,11 +27,11 @@ resource "azurerm_mssql_database" "az_mssql_database" {
 
   depends_on = [azurerm_mssql_server.az_mssql_server]
 
-  name         = "${var.name_prefix}-db"
-  server_id    = azurerm_mssql_server.az_mssql_server.id
-  collation    = var.database.collation
-  max_size_gb  = var.database.max_size_gb
-  sku_name     = var.database.sku_name
+  name        = "${var.name_prefix}-db"
+  server_id   = azurerm_mssql_server.az_mssql_server.id
+  collation   = var.database.collation
+  max_size_gb = var.database.max_size_gb
+  sku_name    = var.database.sku_name
 
   tags = var.tags
 }
