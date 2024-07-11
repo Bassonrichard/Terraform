6eb892b7415b0d24f4ebbc3e1b6018ccd2e9119c
Richard Basson <dvtrb@utilita.co.uk>
2024-07-11 18:34:20 +0200

    added output variables

--- modules/azurerm/mssql_database/output.tf
+++ modules/azurerm/mssql_database/output.tf
@@ -1,3 +1,15 @@
 output "database_password" {
   value = random_password.password.result
 }
+
+output "fully_qualified_domain_name" {
+    value = azurerm_mssql_server.az_mssql_server.fully_qualified_domain_name
+}
+
+output "database_name" {
+  value = azurerm_mssql_database.az_mssql_database.name
+}
+
+output "administrator_login" {
+  value = azurerm_mssql_server.az_mssql_server.administrator_login
+}
