d001d3b55e9235255342e4e52a446ed3404e6206
Richard Basson <bassonrichard@gmail.com>
2025-03-02 10:10:12 +0200

    refactor: Update Cloudflare DNS record module to use a single record object and bump required version

--- modules/cloudflare/dns_record/main.tf
+++ modules/cloudflare/dns_record/main.tf
@@ -1,6 +1,11 @@
 resource "cloudflare_record" "record" {
   zone_id = data.cloudflare_zone.zone.id
-  name    = var.record_name
-  value   = var.record_value
-  type    = var.record_type
+  name    = var.record.name
+  content = var.record.value
+  type    = var.record.type
+  comment = var.record.comment
+  proxied = var.record.proxied 
+
+
+  tags = var.record.tags
 }
